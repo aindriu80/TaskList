6ba64b8824e5a324ce9e3ca9438310d2aa3a8dfc
aindriu80 <aindriu80@gmail.com>
2019-01-16 21:06:32 +0000

    deleting task

--- resources/app.js
+++ resources/app.js
@@ -43,10 +43,25 @@ function addTask(e) {
   // Append li to ul
   taskList.appendChild(li);
 
+  // Store in LS
+  storeTaskInLocalStorage(taskInput.value);
+
   // Clear Input
   taskInput.value = "";
 
   e.preventDefault();
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Remove Task
